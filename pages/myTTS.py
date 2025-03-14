f55b3223f6989bff45d6c38b73b18f24cb2f2e6d
lucete0729 <hongdabin38@gnu.ac.kr>
2025-03-14 21:14:10 +0900

    Update myTTS.py

--- pages/myTTS.py
+++ pages/myTTS.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import qrcode
+
 from PIL import Image
 from wordcloud import WordCloud
 import streamlit.components.v1 as components  # For embedding YouTube videos
